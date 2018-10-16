6583290d2f940ebc1dd01623a1b2b80e749167e4
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-10-16 14:15:06 +0530

    Card Added

--- app/src/main/java/studio/smartters/jewellary/SplashActivity.java
+++ app/src/main/java/studio/smartters/jewellary/SplashActivity.java
@@ -11,12 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this,HomeActivity.class));
-                finish();
-            }
-        },2000);
+        startActivity(new Intent(SplashActivity.this,HomeActivity.class));
+        finish();
     }
 }
